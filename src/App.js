0709155fe704f3644446260fcc62078d84d2f9fe
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-05-10 10:25:47 +0200

    add the css styles and also add the simple login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import "milligram";
 
 
 function App() {
+  const [loggedUser, setLoggedUser] = useState('');
   const [email, setEmail] = useState('fracz@agh.edu.pl');
 
   let message;
@@ -17,13 +19,59 @@ function App() {
       setEmail(event.target.value);
   }
 
-  return (
+  function logUser(email) {
+      setLoggedUser(email);
+  }
+
+  let loginInput = (
       <div>
-          <h1>System do zapisów na zajęcia</h1>
-          <h2>Twój e-mail to {email}.</h2>
           {message}
           <input type="text" value={email} onChange={handleChange}/>
+          <button type="button" onClick={() => logUser(email)}>
+            Zaloguj
+          </button>
       </div>
+  );
+
+  let logoutInput = (
+      <div>
+          <button type="button" onClick={() => logUser('')}>
+            Wyloguj
+          </button>
+      </div>
+  );
+
+  let loggedUserData = (
+      <div>
+          <p>Użytkownik: {loggedUser}</p>
+      </div>
+  );
+  
+
+  return (
+    <div>
+      <h1>System do zapisów na zajęcia</h1>
+      <h2>Twój e-mail to {email}.</h2>
+
+      {loggedUser === '' ? (
+        // Show login form
+        <div>
+          {message}
+          <input type="text" value={email} onChange={handleChange} />
+          <button type="button" onClick={() => logUser(email)}>
+            Zaloguj
+          </button>
+        </div>
+      ) : (
+        // Show logged in info and logout
+        <div>
+          <p>Użytkownik: {loggedUser}</p>
+          <button type="button" onClick={() => logUser('')}>
+            Wyloguj
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
 
