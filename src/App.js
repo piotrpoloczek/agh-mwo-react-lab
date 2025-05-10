2704908be7d2305388912ec9f0595eedca28bb5b
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-05-10 10:00:09 +0200

    add new reactive component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-function App() {
-  let email = 'fracz@agh.edu.pl';
+import { useState } from "react";
 
+
+function App() {
+  const [email, setEmail] = useState('fracz@agh.edu.pl');
+
+  let message;
+  if (email.length < 10) {
+    message = <div>Ale masz krótki adres!</div>;
+  } else if (email.length < 15) {
+    message = <div>Twój adres e-mail jest w sam raz!</div>;
+  } else {
+    message = <div>Twoj adres e-mail jest bardzo długi!</div>;
+  }
+  
   function handleChange(event) {
-      console.log(event.target.value);
+      setEmail(event.target.value);
   }
 
   return (
       <div>
           <h1>System do zapisów na zajęcia</h1>
           <h2>Twój e-mail to {email}.</h2>
-          <input type="text" onChange={handleChange}/>
+          {message}
+          <input type="text" value={email} onChange={handleChange}/>
       </div>
   );
 }
